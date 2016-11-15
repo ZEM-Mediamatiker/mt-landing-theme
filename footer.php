c69514ff1f859ff51bfc01cdf9a07eb6eeb65904
Roger Perren <info@blueforce.ch>
2016-11-15 14:44:25 +0100

    Tracking Code added

--- footer.php
+++ footer.php
@@ -55,5 +55,22 @@
     <script src="<?php echo get_template_directory_uri(); ?>/js/polyfill.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
 
+ <!-- Piwik -->
+ <script type="text/javascript">
+     var _paq = _paq || [];
+     _paq.push(["setDomains", ["*.landing.zem.ch"]]);
+     _paq.push(['trackPageView']);
+     _paq.push(['enableLinkTracking']);
+     (function() {
+         var u="//stats.zem.ch/";
+         _paq.push(['setTrackerUrl', u+'piwik.php']);
+         _paq.push(['setSiteId', '14']);
+         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+         g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+     })();
+ </script>
+ <noscript><p><img src="//stats.zem.ch/piwik.php?idsite=14" style="border:0;" alt="" /></p></noscript>
+ <!-- End Piwik Code -->
+
     </body>
 </html>
