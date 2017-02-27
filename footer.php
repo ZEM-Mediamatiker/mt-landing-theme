190266e7e16f89cbe040b7942e9493444fac276a
zemmediamatiker <lokal@MT-MBP-Eckstein.local>
2017-02-27 15:25:13 +0100

    Piwik to Google Analytics
    Changed analytics tool from Piwik to Google Analytics

--- footer.php
+++ footer.php
@@ -44,22 +44,18 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/polyfill.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
 
- <!-- Piwik -->
- <script type="text/javascript">
-     var _paq = _paq || [];
-     _paq.push(["setDomains", ["*.landing.zem.ch"]]);
-     _paq.push(['trackPageView']);
-     _paq.push(['enableLinkTracking']);
-     (function() {
-         var u="//stats.zem.ch/";
-         _paq.push(['setTrackerUrl', u+'piwik.php']);
-         _paq.push(['setSiteId', '14']);
-         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-         g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-     })();
- </script>
- <noscript><p><img src="//stats.zem.ch/piwik.php?idsite=14" style="border:0;" alt="" /></p></noscript>
- <!-- End Piwik Code -->
+ <!-- Google Analytics -->
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90418745-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+ <!-- End Google Analytics Code -->
 
     </body>
 </html>
